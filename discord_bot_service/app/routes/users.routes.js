9f2dd1be6b9e01b006f82c42f475d09faee33908
Shagun <54307245+Ishika18@users.noreply.github.com>
2022-03-13 23:22:59 -0700

    bug fix redirect "post admin"

--- discord_bot_service/app/routes/users.routes.js
+++ discord_bot_service/app/routes/users.routes.js
@@ -5,83 +5,86 @@ import getAll from "../controllers/requests.controller.js";
 
 var router = express.Router();
 
-router.post("/signup", function (req, res) {
-  sql.query(
-    'UPDATE request SET count = count + 1 WHERE route = "/users/signup";',
-    (err, res) => {
-      if (err) {
-        throw err;
-      }
-    }
-  );
-  let username = req.body.username;
-  let password = req.body.password;
-  console.log(username, password);
+router.post("/signup", function(req, res) {
+    sql.query(
+        'UPDATE request SET count = count + 1 WHERE route = "/users/signup";',
+        (err, res) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(username, password);
 
-  Auth.signUp({
-    username,
-    password,
-  })
-    .then((user) => {
-      console.log(user);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.post("/signin", function (req, res, next) {
-  sql.query(
-    'UPDATE request SET count = count + 1 WHERE route = "/users/signin";',
-    (err, res) => {
-      if (err) {
-        throw err;
-      }
-    }
-  );
-
-  let username = req.body.username;
-  let password = req.body.password;
-
-  Auth.currentAuthenticatedUser()
-    .then(() => {
-      res.redirect(307, `/admin`);
-      next();
-    })
-    .catch((err) => {
-      Auth.signIn(username, password)
+    Auth.signUp({
+            username,
+            password,
+        })
         .then((user) => {
-          console.log(user);
-          const groups =
-            user.signInUserSession.accessToken.payload["cognito:groups"];
-          if (groups.includes("admin")) {
-            getAll(req, res);
-          } else {
-            res.status(401).status({ message: "Unauthorized access" });
-          }
+            console.log(user);
         })
         .catch((err) => {
-          console.log(err);
+            console.log(err);
         });
-    });
 });
 
-router.post("/signout", function (req, res) {
-  sql.query(
-    'UPDATE request SET count = count + 1 WHERE route = "/users/signout";',
-    (err, res) => {
-      if (err) {
-        throw err;
-      }
-    }
-  );
-  Auth.signOut()
-    .then(() => {
-      console.log("signout succcess");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+router.post("/signin", function(req, res, next) {
+    sql.query(
+        'UPDATE request SET count = count + 1 WHERE route = "/users/signin";',
+        (err, res) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    Auth.currentAuthenticatedUser()
+        .then(() => {
+            const groups =
+                user.signInUserSession.accessToken.payload["cognito:groups"];
+            if (groups.includes("admin")) {
+                getAll(req, res);
+            }
+        })
+        .catch((err) => {
+            Auth.signIn(username, password)
+                .then((user) => {
+                    console.log(user);
+                    const groups =
+                        user.signInUserSession.accessToken.payload["cognito:groups"];
+                    if (groups.includes("admin")) {
+                        getAll(req, res);
+                    } else {
+                        res.status(401).status({ message: "Unauthorized access" });
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
+});
+
+router.post("/signout", function(req, res) {
+    sql.query(
+        'UPDATE request SET count = count + 1 WHERE route = "/users/signout";',
+        (err, res) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+    Auth.signOut()
+        .then(() => {
+            console.log("signout succcess");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 
 export default router;
