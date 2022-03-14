9548f748b6065227ae60177a5b8baba46950b4a7
Ryan Leung <ryanleung1004@gmail.com>
2022-03-13 22:10:43 -0700

    Change import error

--- discord_bot_service/app/routes/users.routes.js
+++ discord_bot_service/app/routes/users.routes.js
@@ -1,66 +1,80 @@
-import { Auth } from 'aws-amplify';
-import express from 'express';
-import sql from "./db.js";
+import { Auth } from "aws-amplify";
+import express from "express";
+import sql from "../models/db.js";
 
 var router = express.Router();
 
-const BASE_ROUTE = "/4537/API/V1"
+router.post("/signup", function (req, res) {
+  sql.query(
+    'UPDATE requests SET count = count + 1 WHERE route = "/users/signup";',
+    (err, res) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log(username, password);
 
-router.post('/signup', function(req, res) {
-    sql.query('UPDATE requests SET count = count + 1 WHERE route = "/users/signup";', (err, res) => {
-        if (err) {
-            throw err;
-        }
+  Auth.signUp({
+    username,
+    password,
+  })
+    .then((user) => {
+      console.log(user);
     })
-    let username = req.body.username;
-    let password = req.body.password;
-    console.log(username, password);
-
-    Auth.signUp({
-        username,
-        password
-    }).then(user => {
-        console.log(user);
-    }).catch(err => {
-        console.log(err);
+    .catch((err) => {
+      console.log(err);
     });
 });
 
+router.post("/signin", function (req, res, next) {
+  sql.query(
+    'UPDATE requests SET count = count + 1 WHERE route = "/users/signin";',
+    (err, res) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
 
-router.post('/signin', function(req, res, next) {
-    sql.query('UPDATE requests SET count = count + 1 WHERE route = "/users/signin";', (err, res) => {
-        if (err) {
-            throw err;
-        }
+  let username = req.body.username;
+  let password = req.body.password;
+
+  Auth.currentAuthenticatedUser()
+    .then(() => {
+      res.redirect(307, `/admin`);
+      next();
     })
-
-    let username = req.body.username;
-    let password = req.body.password;
-
-    Auth.currentAuthenticatedUser()
-        .then(() => {
-            res.redirect(307, `/admin`);
-            next();
-        }).catch(err => {
-            Auth.signIn(username, password)
-                .then(user => {
-                    console.log(user);
-                    res.send({ "user": user });
-                }).catch(err => {
-                    console.log(err);
-                });
+    .catch((err) => {
+      Auth.signIn(username, password)
+        .then((user) => {
+          console.log(user);
+          res.send({ user: user });
+        })
+        .catch((err) => {
+          console.log(err);
         });
+    });
+});
 
-})
-
-router.post('/signout', function(req, res) {
-    sql.query('UPDATE requests SET count = count + 1 WHERE route = "/users/signout";', (err, res) => {
-        if (err) {
-            throw err;
-        }
+router.post("/signout", function (req, res) {
+  sql.query(
+    'UPDATE requests SET count = count + 1 WHERE route = "/users/signout";',
+    (err, res) => {
+      if (err) {
+        throw err;
+      }
+    }
+  );
+  Auth.signOut()
+    .then(() => {
+      console.log("signout succcess");
     })
-    Auth.signOut()
-        .then(() => { console.log('signout succcess'); }).catch(err => { console.log(err); });
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 export default router;
