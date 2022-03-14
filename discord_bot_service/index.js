b610f009d2e96030575dd77f0aec9d4bb225ed9d
Shagun <54307245+Ishika18@users.noreply.github.com>
2022-03-13 20:07:22 -0700

    aws login - cors fixed

--- discord_bot_service/index.js
+++ discord_bot_service/index.js
@@ -5,6 +5,9 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import userRoutes from "./app/routes/users.routes.js";
 import adminRoutes from "./app/routes/admin.routes.js";
+import commitmentRoutes from "./app/routes/commitments.routes.js";
+import requestRoutes from "./app/routes/requests.routes.js";
+import cors from "cors";
 
 
 const __filename = fileURLToPath(
@@ -12,17 +15,23 @@ const __filename = fileURLToPath(
 
 // 👇️ "/home/john/Desktop/javascript"
 const __dirname = path.dirname(__filename);
-
 Amplify.configure({...awsExports, ssr: true });
 
 
 let app = express();
+app.use(cors());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 const PORT = 3000;
 const BASE_ROUTE = "/4537/API/V1"
 
 app.use(express.static(path.join(__dirname, '/public')))
 
+
+app.use(`${BASE_ROUTE}/commitment`, commitmentRoutes);
+app.use(`${BASE_ROUTE}/requests`, requestRoutes);
 app.use(`${BASE_ROUTE}/users`, userRoutes);
 app.use(`${BASE_ROUTE}/admin`, adminRoutes);
 app.get(BASE_ROUTE, function(req, res) {
