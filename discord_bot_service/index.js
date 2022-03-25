0ebfd85e5e96b1291c1d3963a9d0286ff07a764c
Shagun <54307245+Ishika18@users.noreply.github.com>
2022-03-25 05:41:23 -0700

    use schedule.routes (index.js)

--- discord_bot_service/index.js
+++ discord_bot_service/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./app/routes/users.routes.js";
 import adminRoutes from "./app/routes/admin.routes.js";
 import commitmentRoutes from "./app/routes/commitments.routes.js";
 import requestRoutes from "./app/routes/requests.routes.js";
+import scheduleRoutes from "./app/routes/schedule.routes.js";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import { readFile } from "fs/promises";
@@ -46,6 +47,7 @@ app.use(`${BASE_ROUTE}/commitment`, commitmentRoutes);
 app.use(`${BASE_ROUTE}/requests`, requestRoutes);
 app.use(`${BASE_ROUTE}/users`, userRoutes);
 app.use(`${BASE_ROUTE}/admin`, adminRoutes);
+app.use(`${BASE_ROUTE}/schedule`, scheduleRoutes);
 app.get(BASE_ROUTE, function (req, res) {
   res.sendFile(__dirname + "/public/admin.html");
 });
