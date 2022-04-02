edef4312c55b37807ed684681c822b8cebccbf39
Ryan Leung <ryanleung1004@gmail.com>
2022-04-02 00:22:32 -0700

    Change database create connection to create pool instead

--- discord_bot_service/app/models/db.js
+++ discord_bot_service/app/models/db.js
@@ -1,27 +1,31 @@
 import mysql from "mysql";
+import util from "util";
 import dbConfig from "../config/db.config.js";
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
 });
 
-const dbConnect = () => {
-  connection.connect((error) => {
-    if (error) throw error;
-    console.log("Successfully connected to the database");
-  });
-
-  connection.on("error", (err) => {
-    console.log("[SQL Error]: ", err);
+// Ping database to check for common exception errors.
+pool.getConnection((err, connection) => {
+  if (err) {
     if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      dbConnect();
+      console.error("Database connection was closed.");
     }
-  });
-};
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.");
+    }
+  }
+  if (connection) connection.release();
+  return;
+});
 
-dbConnect();
+pool.query = util.promisify(pool.query);
 
-export default connection;
+export default pool;
