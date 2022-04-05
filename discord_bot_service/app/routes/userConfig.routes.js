30df3fbd346aa04fe7e62cfaea7dcf917887e073
Ryan Leung <ryanleung1004@gmail.com>
2022-04-04 17:36:01 -0700

    Add auth for userConfig endpoints

--- discord_bot_service/app/routes/userConfig.routes.js
+++ discord_bot_service/app/routes/userConfig.routes.js
@@ -9,10 +9,12 @@ import userConfig from "../controllers/userConfigs.controller.js";
 import { checkForExistingUserConfig } from "../utils/checkForExistingUserConfig.js";
 import { updateSchedule } from "../utils/updateSchedule.js";
 import { incrementEndpointCount } from "../utils/incrementEndpointCount.js";
+import { authorizer } from "../utils/auth.js";
 
 router.put(
   "/:userId",
   incrementEndpointCount,
+  authorizer,
   createUserConfigValidation,
   validate,
   checkForExistingUserConfig,
@@ -21,6 +23,7 @@ router.put(
 router.patch(
   "/:userId/:dayOfWeek",
   incrementEndpointCount,
+  authorizer,
   updateUserConfigValidation,
   validate,
   userConfig.updateUserConfig,
